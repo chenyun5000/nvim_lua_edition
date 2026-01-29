f1eb65b1e8e3458cb776545cb005be428daf4705
chenyun5000 <32665388+chenyun5000@users.noreply.github.com>
2026-01-29 11:51:05 +0800

    Create keymaps.lua

---
+++ keymaps.lua
@@ -0,0 +1,20 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local map = vim.api.nvim_set_keymap
+-- 复用 opt 参数
+local opt = {
+   noremap = true, silent = true
+   }
+
+-- 在visual 模式里粘贴不要复制
+map("v", "p", '"_dP', opt)
+
+-- 退出
+map("n", "q", ":q<CR>", opt)
+map("n", "qq", ":q!<CR>", opt)
+-- map("n", "Q", ":qa!<CR>", opt)
+
+-- insert 模式下，跳到行首行尾
+map("i", "<C-h>", "<ESC>I", opt)
+map("i", "<C-l>", "<ESC>A", opt)
