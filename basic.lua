4744a8ffa89e16c6e9ebcac69d20f2343a302372
chenyun5000 <32665388+chenyun5000@users.noreply.github.com>
2026-01-29 11:50:34 +0800

    Create basic.lua

---
+++ basic.lua
@@ -0,0 +1,40 @@
+-- utf8
+vim.g.encoding = "UTF-8"
+-- vim.o.fileencoding = 'utf-8'
+
+-- 使用相对行号
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+-- 高亮所在行
+vim.wo.cursorline = true
+
+-- 显示左侧图标指示列
+vim.wo.signcolumn = "yes"
+
+-- 右侧参考线，超过表示代码太长了，考虑换行
+vim.wo.colorcolumn = "80"
+
+-- 缩进4个空格等于一个Tab
+vim.o.tabstop = 4
+vim.bo.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftround = true
+
+-- 空格替代tab
+vim.o.expandtab = true
+vim.bo.expandtab = true
+
+-- 新行对齐当前行
+vim.o.autoindent = true
+vim.bo.autoindent = true
+vim.o.smartindent = true
+
+-- 搜索大小写不敏感，除非包含大写
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- 样式
+vim.o.background = "dark"
+vim.o.termguicolors = true
+vim.opt.termguicolors = true
